985833df5273063d71bcfd6f130664b72904d1bc
Manjunath Akalawadi <37879993+Manjunath-Akalawadi@users.noreply.github.com>
2018-08-12 19:12:24 +0530

    initial commit

---
+++ app/src/main/java/com/manju7/demo/Chat.java
@@ -0,0 +1,29 @@
+package com.manju7.chatbot;
+
+public class Chat {
+
+    public String  message;
+    public boolean isSend;
+
+
+    public Chat(String message, boolean isSend) {
+        this.message = message;
+        this.isSend = isSend;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSend() {
+        return isSend;
+    }
+
+    public void setSend(boolean send) {
+        isSend = send;
+    }
+}
